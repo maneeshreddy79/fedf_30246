694370209c55ca21d6dad9cee1792cbeedd3e107
maneeshreddy79 <maneeshkumarreddy79@gmail.com>
2025-10-08 09:10:54 +0530

    Add files via upload

---
+++ WEEK7/data.js
@@ -0,0 +1,6 @@
+export const books = [
+  { title: "⚔️ Baahubali - 1", author: "SS Rajamouli", price: 4909, availability: "in stock" },
+  { title: "🦁 Gen-63", author: "SS Rajamouli", price: 699, availability: "in stock" },
+  { title: "🕶️ Billa", author: "Meher Nolan", price: 59999, availability: "out of stock" },
+  { title: "🔱 Shakti", author: "Boyapati", price: 4500, availability: "in stock" }
+];
